c0704eb59f70447063b433e4139a34e2985b1264
vitali-usik <vitaliy.usik@idt.net>
2019-09-19 17:40:39 +0300

    [m3-t2] added ads generation

---
+++ js/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var ADS_COUNT = 8;
+
+var TITLES = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var TIMES = ['12:00', '13:00', '14:00'];
+var GOODS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+var mapElement = document.querySelector('.map');
+
+var showMap = function () {
+  mapElement.classList.remove('map--faded');
+};
+
+var getRandomFromRange = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getAd = function (index) {
+  var x = getRandomFromRange(0, 1200);
+  var y = getRandomFromRange(130, 630);
+
+  return {
+    author: {
+      avatar: 'img/avatars/user' + (index + 1) + '.png'
+    },
+    offer: {
+      title: TITLES[getRandomFromRange(0, TITLES.length)],
+      address: x + ', ' + y,
+      price: getRandomFromRange(1, 300),
+      type: TYPES[getRandomFromRange(0, TYPES.length)],
+      rooms: getRandomFromRange(1, 4),
+      guests: getRandomFromRange(1, 4),
+      checkin: TIMES[getRandomFromRange(0, TIMES.length)],
+      checkout: TIMES[getRandomFromRange(0, TIMES.length)],
+      features: GOODS, // массив строк случайной длины из ниже предложенных: 'wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner',
+      description: TITLES[getRandomFromRange(0, TITLES.length)],
+      photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg'], // 1 - 13
+    },
+    location: {
+      x: x,
+      y: y
+    }
+  };
+};
+
+var generateAds = function () {
+  var ads = [];
+
+  for (var i = 0; i < ADS_COUNT; i++) {
+    ads.push(getAd(i));
+  }
+
+  return ads;
+};
+
+var init = function () {
+  showMap();
+  var ads = generateAds();
+  console.log('ads', ads);
+};
+
+init();
